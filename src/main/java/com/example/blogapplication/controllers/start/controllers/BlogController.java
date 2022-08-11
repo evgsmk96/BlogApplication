607bc4cb81652d0dacadbd91991a46180c7ea665
evgsmk96 <evgsmk23@mail.ru>
2022-08-11 15:57:36 +0700

    Initial commit

---
+++ src/main/java/com/example/blogapplication/controllers/start/controllers/BlogController.java
@@ -0,0 +1,21 @@
+package com.example.blogapplication.controllers.start.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+
+    @GetMapping("/blog")
+    public String blogMain (Model model) {
+    model.addAttribute("title","Главная страница");
+    return "blog-main";
+}
+
+    @GetMapping("/about")
+    public String aboutUs (Model model) {
+        model.addAttribute("title", "О нас");
+        return "about";
+    }
+}
