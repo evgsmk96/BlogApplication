53833fe96fc69fff93bacbd19d2cc52893963c55
evgsmk96 <evgsmk23@mail.ru>
2022-09-02 11:05:39 +0700

    Repo init

--- src/main/java/com/example/blogapplication/controllers/BlogController.java
+++ src/main/java/com/example/blogapplication/controllers/BlogController.java
@@ -1,17 +1,24 @@
 package com.example.blogapplication.controllers;
 
+import com.example.blogapplication.models.Posts;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import repo.PostRepository;
 
 @Controller
 public class BlogController {
 
+    private PostRepository postRepo;
+
     @GetMapping("/blog")
     public String blogMain (Model model) {
-    model.addAttribute("title","Главная страница");
-    return "blog-main";
-}
+        Iterable<Posts> posts = postRepo.findAll();
+        model.addAttribute("posts", posts);
+        model.addAttribute("title","Главная страница");
+        return "blog-main";
+    }
 
     @GetMapping("/about")
     public String aboutUs (Model model) {
