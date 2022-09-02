e2178a046816f4d49b47c8ad56e67639ce3f4287
evgsmk96 <evgsmk23@mail.ru>
2022-09-02 10:33:19 +0700

    Post Entity init

--- src/main/java/com/example/blogapplication/controllers/BlogController.java
+++ src/main/java/com/example/blogapplication/controllers/BlogController.java
@@ -1,4 +1,4 @@
-package com.example.blogapplication.controllers.start.controllers;
+package com.example.blogapplication.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
