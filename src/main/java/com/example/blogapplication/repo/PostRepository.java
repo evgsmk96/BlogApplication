043ca13934bf163edf7669f804edb173b959e25c
evgsmk96 <evgsmk23@mail.ru>
2022-09-16 14:57:46 +0700

    Repo

---
+++ src/main/java/com/example/blogapplication/repo/PostRepository.java
@@ -0,0 +1,10 @@
+package com.example.blogapplication.repo;
+
+import com.example.blogapplication.models.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Posts, Long> {
+}
